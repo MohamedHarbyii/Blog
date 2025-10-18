bcb8d89e71753bb7ccd37796617ff0fbafd08cd6
MohamedHarbyii <132780965+MohamedHarbyii@users.noreply.github.com>
2025-10-18 14:31:53 +0300

    changes made in the code i used api recourses and used polices to the posts and comments so not any one can delete them

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use  App\Http\Resources;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'=>$this->id,
+            'title'=>$this->title,
+            'post_content'=>$this->content,
+            'created_since'=>$this->created_at->diffForHumans(),
+            'author'=>new UserResource($this->whenLoaded('user')),
+            'comments'=>CommentResource::collection($this->whenLoaded('comment')),
+        ];
+    }
+}
